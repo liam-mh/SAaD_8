46806c22c8332b8cd9f1c2403e3bde77f41ddc0c
Liam <33222279+liam-mh@users.noreply.github.com>
2024-11-04 16:38:44 +0000

    feat/ new func for api-gateway controller

--- Application/api-gateway/controllers/apiController.js
+++ Application/api-gateway/controllers/apiController.js
@@ -11,6 +11,17 @@ const getMembersFromAccountService = async (req, res) => {
     }
 };
 
+const createRecord = async (req, res) => {
+    try {
+        const response = await axios.post(`${config.ACCOUNT_SERVICE_API}/account`);
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error creating user from account service:', error);
+        res.status(500).send('Internal Server Error');
+    }
+};
+
 module.exports = {
-    getMembersFromAccountService
+    getMembersFromAccountService,
+    createRecord
 };
