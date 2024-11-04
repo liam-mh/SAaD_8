549ccd260bccaa1c5708bba9e72f4e028fc97f09
nicklinguy@yahoo.com <nicklinguy@yahoo.com>
2024-11-04 23:06:20 +0000

    updated member router, built bash script and powershell script for running app

--- Application/services/account-service/routes/member/index.js
+++ Application/services/account-service/routes/member/index.js
@@ -1,8 +1,27 @@
+// const express = require('express');
+// const router = express.Router();
+// import MemberController from '../../controllers/memberController';
+
+// // routes
+// router.get('/', memberController.getMembers); // Get all members
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
-const memberController = require('../../controllers/memberController');
+import MemberService from '../../models/service/memberService';
+import MemberController from '../../controllers/memberController';
+const memberEntity = require('../../entity/memberEntity')
 
-// routes
-router.get('/', memberController.getMembers); // Get all members
+// Instantiate MemberService and pass it to MemberController
+const memberService = new MemberService(memberEntity);
+const memberController = new MemberController(memberService);
+
+// Define routes and use memberController to handle requests
+router.get('/', (req, res) => {
+  memberController.readRecords([])
+    .then(members => res.json(members))
+    .catch(error => res.status(500).json({ error: error.message }));
+});
 
 module.exports = router;
