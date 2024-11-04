79b8b9318c215eaed47c6c8cd5928095c0d198b7
Liam <33222279+liam-mh@users.noreply.github.com>
2024-11-04 16:34:25 +0000

    feat/member entity

--- Application/services/account-service/models/entity/memberEntity.js
+++ Application/services/account-service/models/entity/memberEntity.js
@@ -1,25 +1,40 @@
-const fs = require('fs');
-const path = require('path');
+const EntityInterface = require('../../../interfaces/entityInterface');
+const config = require('../../../../azure-db/config/dbConfig');
 
-// Function to read user data from JSON file
-const readMemberData = () => {
-    return new Promise((resolve, reject) => {
-        const filePath = path.join(__dirname, 'testUsers.json'); // Path to your JSON file
+class MemberEntity extends EntityInterface {
+    constructor() {
+        super(config, 'member'); 
+    }
 
-        fs.readFile(filePath, 'utf8', (err, data) => {
-            if (err) {
-                return reject(err);
-            }
-            try {
-                const users = JSON.parse(data);
-                resolve(users);
-            } catch (parseError) {
-                reject(parseError);
-            }
-        });
-    });
-};
+    async createByQuery(memberData) {
+        await this.create(memberData);
+    }
 
-module.exports = {
-    readMemberData
-};
+    async readByQuery() {
+        const members = await this.read();
+        console.log('ENTITY: ',members);
+        return members.length > 0 ? members : null;
+    }
+
+    async getMemberById(memberId) {
+        const members = await this.read('WHERE MemberID = ?', [memberId]);
+        return members.length > 0 ? members[0] : null;
+    }
+
+    async getMemberByName(memberName) {
+        const members = await this.read('WHERE FirstName = ?', [memberName]);
+        return members.length > 0 ? members[0] : null;
+    }
+
+    async updateMember(memberId, updateData) {
+        await this.update(updateData, 'MemberID = ?', [memberId]);
+    }
+
+    async deleteMember(memberId) {
+        await this.delete('MemberID = ?', [memberId]);
+    }
+}
+
+
+
+module.exports = MemberEntity;
