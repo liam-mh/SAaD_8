632517865af9d834111718c285317d12fb4c3f1d
nicklinguy@yahoo.com <nicklinguy@yahoo.com>
2024-11-15 01:09:42 +0000

    Made routing dynamic in account service

--- Application/services/account-service/app.js
+++ Application/services/account-service/app.js
@@ -7,4 +7,4 @@ app.use(express.json());
 app.use('/api', memberRoutes);
 
 const PORT = config.ACCOUNT_SERVICE_PORT;
-app.listen(PORT, () => console.log(`Account Service running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Account Service running on port ${PORT}`)); 
