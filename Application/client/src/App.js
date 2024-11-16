75bd1d78fc52f3fdecb83358f76cabf73510b77b
Liam <33222279+liam-mh@users.noreply.github.com>
2024-11-16 14:06:43 +0000

    feat/session context for app

--- Application/client/src/App.js
+++ Application/client/src/App.js
@@ -1,25 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { SessionContextProvider } from './services/sessionContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
-//pages
+// Pages
 import IndexPage from './pages/index';
 import HelpPage from './pages/help';
 import AccountPage from './pages/account';
 import MediaPage from './pages/media';
+import BasketPage from './pages/basket';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<IndexPage />} />
-        <Route path="/help" element={<HelpPage />} />
-        <Route path="/account" element={<AccountPage />} />
-        <Route path="/media" element={<MediaPage />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <SessionContextProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/help" element={<HelpPage />} />
+          <Route path="/account" element={<AccountPage />} />
+          <Route path="/media" element={<MediaPage />} />
+          <Route path="/basket" element={<BasketPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </SessionContextProvider>
   );
 }
 
