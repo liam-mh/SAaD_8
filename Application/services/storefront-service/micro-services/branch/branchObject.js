d10ebb55c36b9df075cd8fe8bb22b733e7b38944
nicklinguy@yahoo.com <nicklinguy@yahoo.com>
2024-11-16 14:29:44 +0000

    store-front: branch complete

---
+++ Application/services/storefront-service/micro-services/branch/branchObject.js
@@ -0,0 +1,33 @@
+const AbstractObject = require("../../../base-classes/abstractObject");
+
+class BranchObject extends AbstractObject{
+    constructor(){
+        this.BranchID = null;
+        this.firstLineAddress = null;
+        this.poscode = null;
+        this.city = null;
+        this.openingHours = null;
+    }
+    
+    /**
+     * @override
+     * @param {*} arr - array to create a branch object from.
+     */
+    createObjectFromArray(arr) {
+        // Check if array has the expected length to assign to properties.
+        if (arr.length !== 5) {
+        throw new Error("Array must have exactly 5 elements.");
+        }
+
+        // Assign each element in the array to the respective property.
+        [
+            this.BranchID,
+            this.firstLineAddress,
+            this.poscode,
+            this.city,
+            this.openingHours 
+        ] = arr;
+    }
+}
+
+module.exports = BranchObject;
---
+++ Application/services/storefront-service/micro-services/branch/branchService.js
@@ -0,0 +1,13 @@
+const Service = require("../../../base-classes/service");
+const BranchDbHandler = require("./branchDbHandler");
+const BranchObject = require("./branchObject")
+
+class BranchService extends Service{
+    constructor() {
+        const dbHandler = new BranchDbHandler();
+        const object = new BranchObject();
+        super(dbHandler, object);
+      }
+}
+
+module.exports = BranchService;
