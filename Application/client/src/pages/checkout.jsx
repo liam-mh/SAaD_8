f203ed716bd6ee8c08f35c14599c128f2056a9b7
Liam <33222279+liam-mh@users.noreply.github.com>
2024-11-24 00:50:58 +0000

    feat/ map checkout items in table

--- Application/client/src/pages/checkout.jsx
+++ Application/client/src/pages/checkout.jsx
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect } from 'react';
 import LoginCard from '../components/LoginCard';
-import { Button, Container, Row, Col } from 'react-bootstrap';
+import { Button, Container, Row, Col, Table } from 'react-bootstrap';
 import { SessionContext } from '../services/sessionContext';
 
 const CheckoutPage = () => {
-  const { user } = useContext(SessionContext) || {}; 
+  const { user, checkout } = useContext(SessionContext) || {}; 
   return (
     <Container fluid='lg'>
       <h1>Checkout</h1>
@@ -14,7 +14,7 @@ const CheckoutPage = () => {
           {!user ? (
             <LoginCard />
           ) : (
-            <div className='content-panel mb-3'>
+            <div className='content-panel'>
               <Row>
                 <Col>
                   <span>
@@ -39,9 +39,42 @@ const CheckoutPage = () => {
               </Row>
             </div>
           )}
-          <h4 id="order">Order Summary</h4>
+          <h4 id="order" className='mt-3'>Order Summary</h4>
           <div className='content-panel mb-3'>
-            
+            <Table hover className="aml-table">
+              <thead>
+                <tr>
+                  <th>Product</th>
+                  <th>Rent Details</th>
+                </tr>
+              </thead>
+              <tbody>
+                {checkout.map((item, index) => {
+                  const deliveryMessage = item.deliveryOption === 'collect'
+                    ? `In-Store Collection, ${item.branch.Postcode}`
+                    : `Home Delivery, ${user?.Postcode || 'Unknown Address'}`;
+
+                  return (
+                    <tr key={index} style={{ verticalAlign: 'middle' }}>
+                      <td>
+                        <span>
+                          <strong>{item.Title}</strong><br />
+                          Format: {item.Type}<br />
+                          Subtotal: {item.tokens}
+                        </span>
+                      </td>
+                      <td>
+                        <span>
+                          Start: {item.startDate}<br />
+                          Return: {item.returnDate}<br />
+                          Delivery: {deliveryMessage}
+                        </span>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>          
+            </Table>
           </div>
         </Col>
         <Col>
