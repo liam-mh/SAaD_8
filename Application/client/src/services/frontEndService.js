9feb00936227e1e088597c7499f2d02d73e0c036
Liam <33222279+liam-mh@users.noreply.github.com>
2024-11-26 19:04:36 +0000

    update/ autosearch api

--- Application/client/src/services/frontEndService.js
+++ Application/client/src/services/frontEndService.js
@@ -101,14 +101,12 @@ class FrontEndService {
 
   /**
    * Sends an autocomplete request with the provided search characters.
-   *
-   * @param {string} path - The API endpoint path.
    * @param {string} chars - The characters to search for in the autocomplete request.
    * @returns {Promise<Object>} - Resolves to the autocomplete results.
    * @throws {Error} - Throws an error if the API request fails.
    */
-  async autoComplete(path, chars) {
-    const url = `${this.baseRoute}${path}?chars=${encodeURIComponent(chars)}`;
+  async autoComplete(chars) {
+    const url = `${this.baseRoute}/autoComplete?chars=${encodeURIComponent(chars)}`;
     try {
       return await fetchFromApiGateway(url, {
         method: "GET",
