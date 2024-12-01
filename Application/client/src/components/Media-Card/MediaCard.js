5b84f1cffb81d19784c2e7e256f0e1cae0d689ea
nicklinguy@yahoo.com <nicklinguy@yahoo.com>
2024-12-01 11:37:09 +0000

    feat/ added test folders to client and seperated components into ther own dirs

--- Application/client/src/components/Media-Card/MediaCard.js
+++ Application/client/src/components/Media-Card/MediaCard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-const mediaFrontEndService = require('../services/storefront/mediaFrontEndService');
+const mediaFrontEndService = require('../../services/storefront/mediaFrontEndService');
 
 
 function MediaCard({ isSearchResult = false, media }) {
